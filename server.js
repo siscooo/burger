3232f523bc68c0302a6cabf77906c1bce0c9429d
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-29 20:27:33 -0700

    mispelled express

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-var exphbs = require("expreess-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
