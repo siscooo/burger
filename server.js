91e11b222172d92c6dfef36d7e6f0ea8da8266b6
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-29 20:26:36 -0700

    fixed handlebars with the right name

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
-var bodyparser = require("body-parser");
+var PORT = process.env.port || 3000;
+var app = express();
 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var exphbs = require("expreess-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on port:", PORT);
+});
