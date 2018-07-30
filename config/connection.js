2d1485a6125070b3e1fbd60e068cf93c99ea9a51
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-29 22:01:15 -0700

    added heroku credentials

--- config/connection.js
+++ config/connection.js
@@ -15,4 +15,15 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
   });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "rootroot",
+        database: "burger_db"
+    });
+};
+
 module.exports = connection;
