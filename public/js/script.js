5a241078ae07f1bddcc81ec4a900037cc3d3ad50
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-29 20:35:41 -0700

    added script js for buttons

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".devour-form").on("submit", function(evernt) {
+        event.preventDefault();
+
+        var burger_id = $(this).children(".burger_id").val();
+        console.log(burger_id);
+        $.ajax({
+            method: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function(data) {
+            location.reload
+        });
+    });
+});
