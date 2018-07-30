e2f01256a2e6442c911cc7132f0c5e29b6945f8a
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-29 19:35:47 -0700

    burger model finished

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("CHEESEBURGER", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("DOUBLE DOUBLE", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("HAMBURGER", FALSE);
